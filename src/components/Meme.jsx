1fcc1bb4228cd0a9cb146cb4928c7d5c181c9ab7
Javier Figueroa <javiernfigueroa@gmail.com>
2023-08-10 21:52:13 -0400

    day1

---
+++ src/components/Meme.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import memesData from "../memesData";
+
+export default function Meme() {
+  function getMemeImage() {
+    let memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+    </main>
+  );
+}
